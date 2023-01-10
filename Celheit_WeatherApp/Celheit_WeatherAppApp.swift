3ffbb9b19d65639b098c3e10aaaf37fc0b4ca3b9
Jakub Łata <lootjakub@gmail.com>
2023-01-10 17:14:31 +0100

    Initial Commit

---
+++ Celheit_WeatherApp/Celheit_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Celheit_WeatherAppApp.swift
+//  Celheit_WeatherApp
+//
+//  Created by Jakub Łata on 10/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Celheit_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
